0a90d35230d4abb6bf172f013fcb08b9cbaeb174
A-Kija <arvydas.kijakauskas@gmail.com>
2021-05-29 16:34:51 +0300

    02

--- 02/index.php
+++ 02/index.php
@@ -11,19 +11,24 @@ echo ++$trecias;
 echo '<br>';
 echo $trecias;
 echo '<br>';
+
 $sk = 1;
 
+
 echo ++$sk * $sk++;
-// 1 * 3
-
-echo $sk++ * $sk++;
-// 2 * 3
-
-echo $sk++ * ++$sk;
 // 2 * 2
 
-echo ++$sk * ++$sk;
+
+echo $sk++ * $sk++;
 // 1 * 2
+
+
+echo $sk++ * ++$sk;
+// 1 * 3
+
+
+echo ++$sk * ++$sk;
+// 2 * 3
 
 // $ketvirtas = $ketvirtas + 5;
 // $ketvirtas += 5;
@@ -65,6 +70,55 @@ echo $$$pirmas;
 echo '<br>';
 
 var_dump('裡');
+var_dump(0 <=> 8);
+
+
+var_dump(22 == '22'); // 1 == 1 -> true
+
+echo '<br>';
+
+$drambliai = 0;
+if ($drambliai) {
+    echo 'Yra dramblių';
+}
+echo 'Yra dramblių';
+
+
+echo '<br>';
+
+$rezultatas = (false) ? 'A' : ( (true) ? 'B' : 'C' ); // $rezultatas  yra 
+
+
+echo $rezultatas;
+
+echo '<br>';
+
+unset($rezultatas);
+
+// $rezultatas = null;
+
+var_dump(isset($rezultatas));
+
+// echo $rezultatas;
+echo '<br>';
+
+$paukstis = 'Varna';
+
+
+echo $paukstis ?? 'Zyle';
+
+$c = null;
+$d = null;
+
+echo '<br>';
+
+echo ($c ?? 5) * ($d ?? 5);
+
+
+
+
+
+
 
 
 
