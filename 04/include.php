1a2c100675a2a07221d9e0c397fd5c6e8375d00d
A-Kija <arvydas.kijakauskas@gmail.com>
2021-06-07 16:20:56 +0300

    sort

--- 04/include.php
+++ 04/include.php
@@ -5,9 +5,6 @@ echo __DIR__; // iki cia includas
 echo '<br>';
 
 
-
-
-
 // include '01/labas.php'; // Reliatyviai, kas nera labai gerai.
 include_once __DIR__ .'/01/labas.php';
 include_once __DIR__ .'/01/labas.php';
