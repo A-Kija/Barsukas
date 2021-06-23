df6c0f7cb794035ea9e529be78343405d0a48863
A-Kija <arvydas.kijakauskas@gmail.com>
2021-06-23 11:25:26 +0300

    91,92

---
+++ 16/91/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__ . '/Kibiras1.php';
+
+
+$kibirasA = new Kibiras1;
+$kibirasB = new Kibiras1;
+
+_d($kibirasA->kiekPririnktaAkmenu(), 'A');
+_d($kibirasB->kiekPririnktaAkmenu(), 'B');
+
+$kibirasA->prideti1Akmeni();
+$kibirasA->prideti1Akmeni();
+$kibirasB->prideti1Akmeni();
+$kibirasB->pridetiDaugAkmenu(10);
+$kibirasA->pridetiDaugAkmenu(3);
+
+_d($kibirasA->kiekPririnktaAkmenu(), 'A');
+_d($kibirasB->kiekPririnktaAkmenu(), 'B');
+
