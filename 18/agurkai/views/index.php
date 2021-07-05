33036b6f5e345026547e98efe3c79811b7738369
A-Kija <arvydas.kijakauskas@gmail.com>
2021-07-05 12:16:05 +0300

    Agurkai GO

---
+++ 18/agurkai/views/index.php
@@ -0,0 +1,14 @@
+<?php require DIR.'views/top.php' ?>
+<ul>
+    <?php foreach($boxes as $box) : ?>
+        <li>
+            Dėžė Nr. <?= $box['id'] ?> Viso agukų: <?= $box['amount'] ?>
+            [<a href="add/<?= $box['id'] ?>">Pridėti</a>]
+            [<a href="rem/<?= $box['id'] ?>">Išimti</a>]
+            <form action="delete/<?= $box['id'] ?>" method="post">
+            <button type="submit">Išmesti dėžę</button>
+            </form>
+        </li>
+    <?php endforeach ?>
+</ul>
+<?php require DIR.'views/bottom.php' ?>
---
+++ 18/agurkai/views/remove.php
@@ -0,0 +1,8 @@
+<?php require DIR.'views/top.php' ?>
+
+<form action="?rem/<?= $id ?>" method="post">
+    <input type="text" name="amount" value="">
+    <button>Atimti Atimti agurkų</button>
+</form>
+
+<?php require DIR.'views/bottom.php' ?>
