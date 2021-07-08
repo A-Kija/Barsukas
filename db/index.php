99e6843c4a66c5fdd4ac7afb3aab89d35a26d7e1
A-Kija <arvydas.kijakauskas@gmail.com>
2021-07-08 12:28:21 +0300

    db 2

--- db/index.php
+++ db/index.php
@@ -9,7 +9,7 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // kaip masyvas
     PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 $pdo = new PDO($dsn, $user, $pass, $options);
@@ -38,4 +38,42 @@ $sql = "INSERT INTO trees (`name`, height, `type`)
 VALUES ('".$trees[$n][0]."', ".$trees[$n][1].", ".$trees[$n][2].")
 ";
 $pdo->query($sql);
+
+
+//Skaitymas
+//SELECT column1, column2, column3,... FROM table_name
+
+$sql = "SELECT id, `name`, height, `type`
+FROM trees
+WHERE (`type` = 2 AND height > 10) OR (`type` = 1 AND height < 10) OR `type` = 3
+ORDER BY `name` DESC
+-- LIMIT 5
+";
+$stmt = $pdo->query($sql); // DB steitmentas
+while ($row = $stmt->fetch()) // duok man viena eilute
+{
+    echo $row['id'].' '.$row['name'].' '.$row['height'].' '.$row['type'].'<br>';
+}
+
+//Redagavimas
+// UPDATE table_name
+// SET column1=value, column2=value2,...
+// WHERE some_column=some_value 
+$sql = "UPDATE trees
+SET height = 2.20, `name` = 'Mažas Beržas'
+WHERE `name` = 'Beržas'
+";
+$pdo->query($sql);
+
+
+//Trynimas
+// DELETE FROM table_name
+// WHERE some_column = some_value
+$sql = "DELETE FROM trees
+WHERE id = 29 OR 1
+";
+
+// $pdo->query($sql);
+
+
 
